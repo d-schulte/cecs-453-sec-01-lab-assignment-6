6e9a7e7b92f33390b3fb233715af3186fbe88394
Dylan Schulte <dylan.schulte01@student.csulb.edu>
2026-06-23 13:10:16 -0700

    feat: add weather service class to fetch weather data

---
+++ lib/weather_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class WeatherService {
+  Future<Map<String, dynamic>> fetchWeather(String city) async {
+    final apiKey = dotenv.env['API_KEY'];
+    final response = await http.get(
+      Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric',
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
